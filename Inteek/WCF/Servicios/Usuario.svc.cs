777ee5a333e1f672393fe2b2a53797d0784e3bd1
armandofg <armandofg@VIRTUALWINPRO10>
2017-12-27 13:32:00 -0600

    Actualizar Nombre de Espacio del Servicio Usuario.svc

--- Inteek/WCF/Servicios/Usuario.svc.cs
+++ Inteek/WCF/Servicios/Usuario.svc.cs
@@ -9,7 +9,7 @@ using WCF.Entidades;
 using WCF.Contratos.Datos;
 using WCF.Contratos.Operaciones;
 
-namespace WCF
+namespace WCF.Servicios
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
     // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
